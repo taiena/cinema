73df491ed4a111a384c28efab7b91e58eca4994b
Anna Domaskina <taienametztli@mail.ru>
2020-09-09 15:07:20 +0300

    Add movie list to main page

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,11 +1,48 @@
 import React, { Component } from "react";
+import classes from "./Main.module.scss";
+import MovieList from "../../components/MovieList/MovieList";
 
 export default class Main extends Component {
+  state = {
+    error: null,
+    isLoaded: false,
+    items: [],
+  };
+
+  componentDidMount() {
+    const url = "http://localhost:4000";
+    fetch(url + "/films")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.films,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+
   render() {
-    return (
-      <div>
-        <h1>Main page</h1>
-      </div>
-    );
+    const { error, isLoaded, items } = this.state;
+
+    if (error) {
+      return <p>Error {error.message}</p>;
+    } else if (!isLoaded) {
+      return <p>Loading...</p>;
+    } else {
+      return (
+        <div className={classes.Main}>
+          <h1>Main page</h1>
+          <MovieList items={items} />
+        </div>
+      );
+    }
   }
 }
