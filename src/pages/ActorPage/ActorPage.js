bdd05ad33f23d707388e943fd4307fd7997615f0
Anna Domaskina <taienametztli@mail.ru>
2020-09-27 15:39:38 +0300

    changed fetch to axios on actor page & movie page

--- src/pages/ActorPage/ActorPage.js
+++ src/pages/ActorPage/ActorPage.js
@@ -1,47 +1,30 @@
 import React, { Component } from "react";
 import classes from "./ActorPage.module.scss";
+import * as axios from "axios";
 
 export default class ActorPage extends Component {
   state = {
-    error: null,
-    isLoaded: false,
     item: {},
   };
 
   componentDidMount() {
     const url = "http://localhost:4000";
     const actorUrl = this.props.match.params.id;
-    fetch(url + "/actors/" + actorUrl)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            item: result.actor,
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error,
-          });
-        }
-      );
+
+    axios.get(url + "/actors/" + actorUrl).then((response) => {
+      this.setState({
+        item: response.data.actor,
+      });
+    });
   }
   render() {
-    const { error, isLoaded, item } = this.state;
+    const { item } = this.state;
 
-    if (error) {
-      return <p>Error {error.message}</p>;
-    } else if (!isLoaded) {
-      return <p>Loading...</p>;
-    } else {
-      return (
-        <div className={classes.ActorPage}>
-          <h1>Actor page</h1>
-          <h2>{item.name}</h2>
-        </div>
-      );
-    }
+    return (
+      <div className={classes.ActorPage}>
+        <h1>Actor page</h1>
+        <h2>{item.name}</h2>
+      </div>
+    );
   }
 }
--- src/pages/MoviePage/MoviePage.js
+++ src/pages/MoviePage/MoviePage.js
@@ -1,47 +1,30 @@
 import React, { Component } from "react";
+import * as axios from "axios";
 
 export default class MoviePage extends Component {
   state = {
-    error: null,
-    isLoaded: false,
     item: {},
   };
 
   componentDidMount() {
     const url = "http://localhost:4000";
     const filmUrl = this.props.match.params.id;
-    fetch(url + "/films/" + filmUrl)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            item: result.film,
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error,
-          });
-        }
-      );
+
+    axios.get(url + "/films/" + filmUrl).then((response) => {
+      this.setState({
+        item: response.data.film,
+      });
+    });
   }
   render() {
-    const { error, isLoaded, item } = this.state;
+    const { item } = this.state;
 
-    if (error) {
-      return <p>Error {error.message}</p>;
-    } else if (!isLoaded) {
-      return <p>Loading...</p>;
-    } else {
-      return (
-        <div>
-          <h1>Movie page</h1>
-          <h2>{item.title}</h2>
-          <div>{item.production_year}</div>
-        </div>
-      );
-    }
+    return (
+      <div>
+        <h1>Movie page</h1>
+        <h2>{item.title}</h2>
+        <div>{item.production_year}</div>
+      </div>
+    );
   }
 }
