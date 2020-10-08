beb1833804154f2030caa8566d1b18519bc74508
Anna Domaskina <taienametztli@mail.ru>
2020-10-08 14:53:03 +0300

    added thunk for genre page

--- src/pages/GenrePage/GenrePageContainer.js
+++ src/pages/GenrePage/GenrePageContainer.js
@@ -1,35 +1,19 @@
 import React, { Component } from "react";
 import GenrePage from "./GenrePage";
-import {
-  setFilms,
-  toggleIsLoading,
-  setGenreUrl,
-} from "../../redux/genreReducer";
+import { getFilmsGenre } from "../../redux/genreReducer";
 import { connect } from "react-redux";
 import Preloader from "../../components/UI/Preloader/Preloader";
-import { filmsGenreAPI } from "../../api/api";
 
 class GenrePageContainer extends Component {
   componentDidMount() {
-    this.props.toggleIsLoading(true);
     const genreUrl = this.props.match.params.name;
-
-    filmsGenreAPI.getFilmsGenre(genreUrl).then((data) => {
-      this.props.toggleIsLoading(false);
-      this.props.setFilms(data.films);
-    });
+    this.props.getFilmsGenre(genreUrl);
   }
 
   componentDidUpdate(prevProps, prevState) {
     const genreUrl = this.props.match.params.name;
     if (prevProps.genreUrl !== genreUrl) {
-      this.props.toggleIsLoading(true);
-
-      filmsGenreAPI.getFilmsGenre(genreUrl).then((data) => {
-        this.props.toggleIsLoading(false);
-        this.props.setGenreUrl(genreUrl);
-        this.props.setFilms(data.films);
-      });
+      this.props.getFilmsGenre(genreUrl);
     }
   }
 
@@ -55,7 +39,5 @@ let mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, {
-  setFilms,
-  toggleIsLoading,
-  setGenreUrl,
+  getFilmsGenre,
 })(GenrePageContainer);
--- src/redux/genreReducer.js
+++ src/redux/genreReducer.js
@@ -1,3 +1,5 @@
+import { filmsGenreAPI } from "../api/api";
+
 const SET_FILMS = "SET_FILMS";
 const TOGGLE_IS_LOADING = "TOGGLE_IS_LOADING";
 const SET_GENRE_URL = "SET_GENRE_URL";
@@ -43,5 +45,18 @@ export const setGenreUrl = (genreUrl) => ({
   type: SET_GENRE_URL,
   genreUrl,
 });
+
+//thunk
+export const getFilmsGenre = (genreUrl) => {
+  return (dispatch) => {
+    dispatch(toggleIsLoading(true));
+
+    filmsGenreAPI.getFilmsGenre(genreUrl).then((data) => {
+      dispatch(toggleIsLoading(false));
+      dispatch(setFilms(data.films));
+      dispatch(setGenreUrl(genreUrl));
+    });
+  };
+};
 
 export default genreReducer;
